f9725fcba6880d1383dc3af94eb4c5fd48497bba
S.R. Avinash Maddikonda <147003218+avinashmsr@users.noreply.github.com>
2023-10-21 19:20:02 -0400

    Update models.py
    adding primary keys and other column updates

--- RailSafety_MVP_BackEndCode/wspd_app/models.py
+++ RailSafety_MVP_BackEndCode/wspd_app/models.py
@@ -5,20 +5,20 @@ User = get_user_model()
 
 
 class Train(models.Model):
-    train_no = models.IntegerField()
+    train_no = models.IntegerField(primary_key=True)
     train_name = models.CharField(max_length=264)
     description = models.CharField(max_length=264)
 
 
 class Rake(models.Model):
     train = models.ForeignKey(Train, related_name='rakes', on_delete=models.CASCADE)
-    rake_no = models.IntegerField()
+    rake_no = models.CharField(primary_key=True, max_length=50)
     description = models.CharField(max_length=264)
     status = models.CharField(max_length=20)
 
 
 class Coach(models.Model):
-    coach_no = models.IntegerField()
+    coach_no = models.CharField(primary_key=True, max_length=50)
     coach_type = models.CharField(max_length=264)
     coach_class = models.CharField(max_length=264)
     description = models.CharField(max_length=264)
@@ -36,8 +36,5 @@ class Coach(models.Model):
 
 class FaultTable(models.Model):
     wsp_status_code = models.IntegerField(default=0)
-    fault_datetime = models.DateField()
-    device_id = models.IntegerField(default=0)
-    train = models.ForeignKey(Train, related_name='train_level_faults', on_delete=models.CASCADE)
-    rake = models.ForeignKey(Rake, related_name='rake_level_faults', on_delete=models.CASCADE)
-    coach = models.ForeignKey(Coach, related_name='coach_level_faults', on_delete=models.CASCADE)
+    fault_datetime = models.DateTimeField()
+    coach = models.ForeignKey(Coach, related_name='faults', on_delete=models.CASCADE)
